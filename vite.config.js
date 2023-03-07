58281d0aa67c767615377484f856f2558c8da25e
magdy-serve <124213911+magdy-serve@users.noreply.github.com>
2023-03-07 08:07:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 {
-  "version": 2,
   "builds": [
     {
       "src": "public/index.php",
@@ -8,8 +7,22 @@
   ],
   "routes": [
     {
+      "src": "/api/test.php",
+      "dest": "/api/test.php"
+    },
+    {
       "src": "/(.*)",
       "dest": "/public/index.php"
     }
-  ]
+  ],
+  "functions": {
+    "api/test.php": {
+      "runtime": "@vercel/php",
+      "memory": 128,
+      "maxDuration": 5000,
+      "includeFiles": [
+        "api/test.php"
+      ]
+    }
+  }
 }
