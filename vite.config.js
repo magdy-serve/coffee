752f1a137a8705316ed40d1818f996ec15bbd318
magdy-serve <124213911+magdy-serve@users.noreply.github.com>
2023-03-07 08:30:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -24,5 +24,19 @@
         "api/test.php"
       ]
     }
-  }
+  },
+      import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
 }
